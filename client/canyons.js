890f81a262c91d38bbba3ae70c2cfb55e99f4e0c
Sam Gibson <sam@ifdown.net>
2015-03-15 17:29:10 +1100

    Fixes server-side rendering w/ client
    Note: Don't attach to the ID that the server generates, instead attach to a parent

--- client/canyons.js
+++ client/canyons.js
@@ -39,7 +39,7 @@ module.exports = {
         form: function() {
             React.render(
                 React.createElement(Form, {}),
-                document.getElementById("new-canyon")
+                document.getElementById("canyons-form")
             );
         }
     }
